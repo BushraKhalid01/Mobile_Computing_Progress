ef21fda3831bbd81f6e3cf58f2a354bc3db9692e
BushraKhalid01 <bsef18m018@pucit.edu.pk>
2021-04-06 20:03:09 +0500

    Dialler and Link adding practice

---
+++ Task1/app/src/main/java/org/haqnawaz/task1/MainActivity.java
@@ -0,0 +1,21 @@
+package org.haqnawaz.task1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Move(View view){
+        Intent intent=new Intent(this,NewActivity.class);
+        startActivity(intent);
+    }
+}
